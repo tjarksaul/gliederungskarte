d34580e9ca577cf6f1191c8686eb3d867544a9d8
Tjark Saul <mail@tjarksaul.de>
2020-03-23 20:39:12 +0100

    Disallow zooming without modifier key pressed

--- src/map/touch.ts
+++ src/map/touch.ts
@@ -12,29 +12,45 @@ function isTwoFingerTouch(e: MapboxEvent<TouchEvent>): boolean {
 
 let timeout: number | undefined
 
-function setTouchInfoVisible(visible: boolean): void {
-  const state = visible ? 'flex' : 'none'
+function hideScrollInfo(): void {
   const touchInfo = document.getElementById('touchInfo')
   if (touchInfo) {
-    touchInfo.style.display = state
+    touchInfo.style.display = 'none'
   }
 
   window.clearTimeout(timeout)
 }
 
-function showTouchInfoWithTimeout(): void {
+function showScrollInfo(touch: boolean): void {
+  const touchInfo = document.getElementById('touchInfo')
+  if (touchInfo) {
+    let text = 'Zum Bewegen der Karte mit zwei Fingern berühren.'
+
+    if (!touch) {
+      const macOS = navigator.platform.toLowerCase().indexOf('mac') >= 0
+      const key = macOS ? '⌘' : 'Strg'
+      text = `Zum Zoomen der Karte bei gedrückter ${key}-Taste scrollen.`
+    }
+
+    touchInfo.innerText = text
+
+    touchInfo.style.display = 'flex'
+  }
+}
+
+function showScrollInfoWithTimeout(touch = true): void {
   window.clearTimeout(timeout)
 
-  setTouchInfoVisible(true)
-  window.setTimeout(() => {
-    setTouchInfoVisible(false)
+  showScrollInfo(touch)
+  timeout = window.setTimeout(() => {
+    hideScrollInfo()
   }, 3000)
 }
 
 export function disableOneFingerTouch(map: Map): Map {
   map.on('dragstart', event => {
     if (isTouchEvent(event) && !isTwoFingerTouch(event)) {
-      showTouchInfoWithTimeout()
+      showScrollInfoWithTimeout()
       map.dragPan.disable()
     }
   })
@@ -42,9 +58,20 @@ export function disableOneFingerTouch(map: Map): Map {
   // Drag events not emited after dragPan disabled, so I use touch event here
   map.on('touchstart', event => {
     if (isTouchEvent(event) && isTwoFingerTouch(event)) {
-      setTouchInfoVisible(false)
+      hideScrollInfo()
       map.dragPan.enable()
     }
+  })
+
+  map.on('wheel', event => {
+    const { ctrlKey, altKey, metaKey } = event.originalEvent
+
+    if (ctrlKey || metaKey || altKey) {
+      return
+    }
+
+    showScrollInfoWithTimeout(false)
+    event.preventDefault()
   })
 
   return map
